27ec270cdb70477c5ac75c26e3f1a5b4f1cc0167
Abdul Razak <moabdulrazak17@gmail.com>
2025-09-15 15:49:50 +0000

    chore: react-query setup

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,32 @@
+import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanstackRouter } from "@tanstack/react-router";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 
+import { DefaultCatchBoundary } from "./components/default-catch-boundary";
+import { NotFound } from "./components/not-found";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 export const createRouter = () => {
-  return createTanstackRouter({
+  const queryClient = new QueryClient();
+
+  const router = createTanstackRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    context: { queryClient },
+    defaultPreload: "intent",
+    defaultErrorComponent: DefaultCatchBoundary,
+    defaultNotFoundComponent: () => <NotFound />,
   });
+
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
+  });
+
+  return router;
 };
 
 // Register the router instance for type safety
