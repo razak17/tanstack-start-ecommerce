a997dec097028671e868a9559d112e6f1e523c03
Abdul Razak <moabdulrazak17@gmail.com>
2025-09-18 02:15:54 +0000

    chore: prepare for netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,42 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
-const config = defineConfig({
-  plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart({
-      customViteReactPlugin: true,
-    }),
-    viteReact(),
-  ],
+import pkg from './package.json'
+
+const config = defineConfig(async ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...import.meta.env,
+    ...loadEnv(mode, process.cwd(), ''),
+  }
+
+  await import('./src/lib/env/server')
+  await import('./src/lib/env/client')
+
+  return {
+    plugins: [
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+      tailwindcss(),
+      tanstackStart({
+        customViteReactPlugin: true,
+        target: 'netlify',
+      }),
+      viteReact(),
+    ],
+    define: {
+      APP_VERSION: JSON.stringify(pkg.version),
+    },
+  }
 })
 
 export default config
+
+declare global {
+  const APP_VERSION: string
+}
